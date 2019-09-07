6288b3e1ef5d76dfc3ae713f3050fe59f9f88bf3
kARVAJJJJJJ <illusion1227@gmail.com>
2019-09-07 18:51:39 +0200

    Magic placeholder

--- StockManagementSW/Program.cs
+++ StockManagementSW/Program.cs
@@ -18,5 +18,13 @@ namespace StockManagementSW
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
         }
+
+        // New function
+        static void GitTest()
+        {
+            // Do magic here later
+            // Kavin please remove this
+        }
+
     }
 }
